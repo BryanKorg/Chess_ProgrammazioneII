e317301d5ee418710b5cdd55fa5eb51090e0ef53
BryanKorg <Sam@MacBook-Pro-di-Samuele.local>
2015-08-08 15:36:39 +0200

    Modificato Initial positions

--- src/model/Model.java
+++ src/model/Model.java
@@ -5,7 +5,11 @@ import view.View;
 public interface Model {
 
 	
-	
+	/**
+	 * Commento a caso
+	 * @param x
+	 * @param y
+	 */
 	public void MoveCheck(int x,int y);
 	public void setView(View view);
 	public void SetUpgradedPawn(int x,int y,int value);
