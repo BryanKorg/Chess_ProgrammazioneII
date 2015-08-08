ba47b150e4ca27f00067f26ce69faa9449b0cfdf
HGS-Andry <HGS-Andry@users.noreply.github.com>
2015-08-08 15:55:29 +0200

    Aggiunta commenti sul model

--- src/model/Model.java
+++ src/model/Model.java
@@ -3,10 +3,25 @@ package model;
 import view.View;
 
 public interface Model {
-
 	
-	
+	/**
+	 * Effettua un cambiamento date le coordinate del quadrato scelto
+	 * @param x riga
+	 * @param y colonna
+	 */
 	public void MoveCheck(int x,int y);
+	
+	/**
+	 * Setta la view
+	 * @param view
+	 */
 	public void setView(View view);
-	public void SetUpgradedPawn(int x,int y,int value);
+	
+	/**
+	 * Setta il pezzo per fare la promozione del pedone
+	 * @param x
+	 * @param y
+	 * @param value
+	 */
+	public void SetUpgradedPawn(int x,int y,int value);  //TODO deve essere di tipo piece?
 }
